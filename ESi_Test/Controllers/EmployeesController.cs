e8e84cdfd8d0539b0931f52b45fb0abe18f6a21b
Sunny Cheng <005haru@gmail.com>
2022-11-24 03:47:06 +0800

    basic feature

---
+++ ESi_Test/Controllers/EmployeesController.cs
@@ -0,0 +1,47 @@
+﻿using ESi_Test.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ESi_Test.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly NorthwindContext _context;
+        public EmployeesController(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(string ReportsTo)
+        {
+            if(ReportsTo == null)
+            {
+                return View(new List<Employee>());
+            }
+            else
+            {
+                int reportsId;
+                bool success = int.TryParse(ReportsTo, out reportsId);
+
+                if (success)
+                {
+                    List<Employee> employees =  _context.Employees.Where(e => e.ReportsTo == reportsId).ToList();
+                    List<Employee> subEmployees = new List<Employee>();
+                    foreach (var em in employees)
+                    {
+                        subEmployees = subEmployees.Union(_context.Employees.Where(e => e.ReportsTo == em.EmployeeId)).ToList();
+                    }
+                    
+                    var result = employees.Union(subEmployees).OrderBy(e=>e.EmployeeId).ToList();
+                    return View(result);
+                }
+                else
+                {
+                    return RedirectToAction("Error", "Home");
+                }
+            }
+
+        }
+    }
+}
---
+++ ESi_Test/Models/NorthwindContext.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace ESi_Test.Models
+{
+    public partial class NorthwindContext : DbContext
+    {
+        public NorthwindContext()
+        {
+        }
+
+        public NorthwindContext(DbContextOptions<NorthwindContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; }
+
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Chinese_Taiwan_Stroke_CI_AS");
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasIndex(e => e.LastName, "LastName");
+
+                entity.HasIndex(e => e.PostalCode, "PostalCode");
+
+                entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+
+                entity.Property(e => e.Address).HasMaxLength(60);
+
+                entity.Property(e => e.BirthDate).HasColumnType("datetime");
+
+                entity.Property(e => e.City).HasMaxLength(15);
+
+                entity.Property(e => e.Country).HasMaxLength(15);
+
+                entity.Property(e => e.Extension).HasMaxLength(4);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(10);
+
+                entity.Property(e => e.HireDate).HasColumnType("datetime");
+
+                entity.Property(e => e.HomePhone).HasMaxLength(24);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(20);
+
+                entity.Property(e => e.Notes).HasColumnType("ntext");
+
+                entity.Property(e => e.Photo).HasColumnType("image");
+
+                entity.Property(e => e.PhotoPath).HasMaxLength(255);
+
+                entity.Property(e => e.PostalCode).HasMaxLength(10);
+
+                entity.Property(e => e.Region).HasMaxLength(15);
+
+                entity.Property(e => e.Title).HasMaxLength(30);
+
+                entity.Property(e => e.TitleOfCourtesy).HasMaxLength(25);
+
+                entity.HasOne(d => d.ReportsToNavigation)
+                    .WithMany(p => p.InverseReportsToNavigation)
+                    .HasForeignKey(d => d.ReportsTo)
+                    .HasConstraintName("FK_Employees_Employees");
+            });
+
+            modelBuilder.HasSequence<int>("InvoiceSeq");
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
